9240c77370bfbbcf13737713a16b90db77c23ad2
sedacuenta <sedacuenta7777@gmail.com>
2024-07-29 17:37:37 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
+
+mongoose.model('User', UserSchema);
 
