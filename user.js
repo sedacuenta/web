00effc8d45af1930aeeaea4ddd3f0d901e8b447f
sedacuenta <sedacuenta7777@gmail.com>
2024-07-29 17:33:56 +0200

    Create user.js

---
+++ user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  googleId: { type: String, required: true },
+  displayName: { type: String, required: true },
+  image: { type: String }
+});
+
+module.exports = mongoose.model('User', UserSchema);
